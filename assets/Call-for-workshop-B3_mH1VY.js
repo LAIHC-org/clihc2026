16af40ffa8eea84129bebdd58db98c369c7ced2d
Mike Roguez <mike.roguez@gmail.com>
2026-02-23 15:48:17 -0600

    Updates

--- assets/Call-for-workshop-B3_mH1VY.js
+++
@@ -1,1 +0,0 @@
-import{O as o}from"./OrganizerItem-D0RK9gwd.js";import{_ as e,o as r,c as s,T as t}from"./index-BjPwRtw8.js";const n={components:{OrganizerItem:o},setup(){return{Tr:t}}},a={class:"mt-6 text-center"};function c(p,l,_,i,m,f){return r(),s("p",a,"The Call for Workshops and Tutorials will be announced soon.")}const h=e(n,[["render",c]]);export{h as default};
