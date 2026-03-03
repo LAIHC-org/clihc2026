7c95cee137666b7f7eb7c979bd0c36161f7cdd27
Mike Roguez <mike.roguez@gmail.com>
2026-03-02 21:58:00 -0600

    Updates

--- assets/Accessibility-authors-BPFTLzuA.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c}from"./index-DDD3JYs4.js";const t={},o={class:"mt-6 text-center"};function n(r,a){return s(),c("p",o,"Las Recomendaciones de Accesibilidad para Autores estarán disponibles pronto.")}const _=e(t,[["render",n]]);export{_ as default};
